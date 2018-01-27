3cfc6a097b08a0d07e9ebb3584433cef02940cc6
Jakub Rumpel <jakub@rumpel.pl>
2018-01-27 01:39:20 +0100

    Jumping + Moving + Canvas Text working
    Jumping now wort at maybe too much power, so it's to look at. A lot has changed in movement, and adding canvas text real time chaning is letting me debug stuff easier. Yay!

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -1,18 +1,68 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
 
-	public Rigidbody2D
-	
+	public Rigidbody2D Player;
+	public Text HorAx;
+	public Text VerAx;
+	public int Speed = 5;
+	public int JumpPower = 20;
+	public bool Ground = true;
+
 	// Use this for initialization
-	void Start () {
+	void Start ()
+	{ 
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void FixedUpdate ()
+	{
+		Vector2 move = new Vector2(Input.GetAxis("Horizontal"),0f);
+		Vector2 jump = new Vector2(0f, Player.velocity.y);
+
 		
+		
+		if (Input.GetButtonDown("Jump") && Ground == true)
+		{
+			Debug.Log((Ground));
+			jump = Vector2.up * JumpPower;
+			InvokeRepeating("Falsing",0.5f,0.1f);
+			Debug.Log((Ground));
+
+
+			//Debug.Log(Ground);
+		}
+		
+		
+		
+		Player.velocity = move * Speed + jump;
+
+		
+		
+		HorAx.text = "X Velocity: " + Player.velocity.x;
+		VerAx.text = "Y Velocity: " + Player.velocity.y;
+	}
+
+	private void OnCollisionStay2D(Collision2D other)
+	{
+		Ground = true;
+	}
+
+	private void OnCollisionEnter2D(Collision2D other)
+	{
+		if (other.gameObject.CompareTag("Platform"))
+		{
+			//Debug.Log(Ground);
+		}
+	}
+
+	void Falsing()
+	{
+		Ground = false;
+		Debug.Log(Ground);
 	}
 }
